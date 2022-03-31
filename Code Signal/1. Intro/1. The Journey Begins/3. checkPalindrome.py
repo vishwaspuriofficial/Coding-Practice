a0355620970de4b63ab43daa9f98a32a3426c0bd
vishwaspuriofficial@gmail.com <80814270+vishwaspuriofficial@users.noreply.github.com>
2022-03-31 08:31:13 -0400

    .

---
+++ Code Signal/1. Intro/1. The Journey Begins/3. checkPalindrome.py
@@ -0,0 +1,7 @@
+# https://app.codesignal.com/arcade/intro/level-1/s5PbmwxfECC52PWyQ
+
+def solution(inputString):
+    if inputString == inputString[::-1]:
+        return True
+    else:
+        return False
