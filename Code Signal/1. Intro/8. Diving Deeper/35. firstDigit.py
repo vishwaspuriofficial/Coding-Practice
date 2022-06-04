b70c30029ea14a7ad2d52c41750bc30dbcc67561
vishwaspuriofficial@gmail.com <80814270+vishwaspuriofficial@users.noreply.github.com>
2022-06-03 20:23:47 -0400

    .

---
+++ Code Signal/1. Intro/8. Diving Deeper/35. firstDigit.py
@@ -0,0 +1,9 @@
+#https://app.codesignal.com/arcade/intro/level-8/rRGGbTtwZe2mA8Wov
+
+def solution(inputString):
+    for i in inputString:
+        try:
+            a = int(i)
+            return i
+        except:
+            pass
