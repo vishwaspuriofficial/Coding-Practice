597a56431fa614d9e8a679a2fcf169cfc0edf9a7
vishwaspuriofficial@gmail.com <80814270+vishwaspuriofficial@users.noreply.github.com>
2022-06-12 14:07:46 -0400

    .

---
+++ Code Signal/1. Intro/11. Rainbow of Clarity/48. isDigit.py
@@ -0,0 +1,8 @@
+#https://app.codesignal.com/arcade/intro/level-11/Zr2XXEpkBPtYWqPJu
+
+def solution(symbol):
+    try:
+        symbol = int(symbol)
+        return True
+    except:
+        return False
