25cc83e67901bce7f1ebe7c16a0befb513265b6d
vishwaspuriofficial@gmail.com <80814270+vishwaspuriofficial@users.noreply.github.com>
2022-10-03 20:08:51 -0400

    .

---
+++ Hacker Rank/1 Month Preparation Kit/Week 2/Grid Challenge.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gridChallenge' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING_ARRAY grid as parameter.
+#
+
+def gridChallenge(grid):
+    # Write your code here
+    for i in range(len(grid)):
+        grid[i] = sorted(grid[i])
+    a = [[] for l in grid[0]]
+    for j in range(len(grid[0])):
+        for k in range(len(grid)):
+            a[j].append(grid[k][j])
+    for n in a:
+        if sorted(n) != n:
+            return "NO"
+    return "YES"
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        grid = []
+
+        for _ in range(n):
+            grid_item = input()
+            grid.append(grid_item)
+
+        result = gridChallenge(grid)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
